34c24711ed567101d0eb64d8becda5abd923c2d4
Shkliar Maksym <shkliar.maksym@chnu.edu.ua>
2023-04-23 23:09:27 +0300

    Зробив С і D (D знаходиться в server.test)

--- A.js
+++ A.js
@@ -39,3 +39,25 @@ app.post('/users/:userId/todos', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+
+//Middleware
+function performanceMiddleware(req, res, next) {
+    const start = process.hrtime();
+
+    res.on('finish', () => {
+        const end = process.hrtime(start);
+        const duration = (end[0] * 1000) + (end[1] / 1000000);
+
+        console.log(`Endpoint '${req.method} ${req.url}' completed in ${duration.toFixed(2)} ms`);
+    });
+
+    next();
+}
+
+
+app.get('/users/:userId/posts', performanceMiddleware, async (req, res) => {
+});
+
+app.post('/users/:userId/posts', performanceMiddleware, async (req, res) => {
+
+});
